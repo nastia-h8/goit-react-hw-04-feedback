aa2a17e6891285a23b1e145d18dda31a4dde644b
Anastasiia Haliara <an.haliara@gmail.com>
2023-09-07 11:54:38 +0300

    add useReducer example

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,7 +23,7 @@ export function App() {
   };
 
   const options = Object.keys(feedback);
-  const total = Object.values(feedback).reduce((total, opt) => total + opt, 0);
+  const total = Object.values(feedback).reduce((acc, value) => acc + value, 0);
   const positivePercentage =
     feedback.good > 0 ? Math.round((feedback.good / total) * 100) : 0;
 
@@ -52,3 +52,71 @@ export function App() {
     </Layout>
   );
 }
+
+// useReducer
+// import { useReducer } from 'react';
+
+// import { Statistics } from 'components/Statistics/Statistics';
+// import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+// import { Section } from 'components/Section/Section';
+// import { Notification } from 'components/Notification/Notification';
+
+// import { Layout } from 'components/Layout';
+// import { GlobalStyle } from 'components/GlobalStyle';
+
+// function reducer(prevState, action) {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case 'good':
+//       return { ...prevState, good: prevState.good + payload };
+//     case 'neutral':
+//       return { ...prevState, neutral: prevState.neutral + payload };
+//     case 'bad':
+//       return { ...prevState, bad: prevState.bad + payload };
+//     default:
+//       return console.log(`Unsupported type - ${type}`);
+//   }
+// }
+
+// export function App() {
+//   const [state, dispatch] = useReducer(reducer, {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   });
+
+//   const updateFeedbackStats = option => {
+//     dispatch({ type: option, payload: 1 });
+//   };
+
+//   const options = Object.keys(state);
+//   const total = Object.values(state).reduce((acc, value) => acc + value, 0);
+//   const positivePercentage =
+//     state.good > 0 ? Math.round((state.good / total) * 100) : 0;
+
+//   return (
+//     <Layout>
+//       <Section title="Please leave feedback">
+//         <FeedbackOptions
+//           options={options}
+//           onLeaveFeedback={updateFeedbackStats}
+//         />
+//       </Section>
+
+//       <Section title="Statistics">
+//         {total > 0 ? (
+//           <Statistics
+//             feedback={state}
+//             total={total}
+//             positivePercentage={positivePercentage}
+//           />
+//         ) : (
+//           <Notification message="There is no feedback yet" />
+//         )}
+//       </Section>
+
+//       <GlobalStyle />
+//     </Layout>
+//   );
+// }
